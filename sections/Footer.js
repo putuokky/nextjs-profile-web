3985fd5a5a78525dade1912a95bc03e0833c66c9
Okky Maheswara <okkymahes@gmail.com>
2022-06-24 19:26:15 +0800

    ganti footer

--- sections/Footer.js
+++ sections/Footer.js
@@ -2,7 +2,7 @@
 export default function Footer() {
   return (
     <footer className="py-6 text-center text-gray-500 dark:text-gray-400 text-sm">
-      <span className="dark:text-gray-100 text-gray-900 font-bold text-md mr-2">Lutfi</span>&copy; {new Date().getFullYear()} All Right Reversed
+      <span className="dark:text-gray-100 text-gray-900 font-bold text-md mr-2">Lutfi</span>&copy; {new Date().getFullYear()} All Right Reserved
     </footer>
   )
 }
