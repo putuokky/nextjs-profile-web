b05a699886129ea16bb5ed2bbdaec249f1e4718a
Okky Maheswara <okkymahes@gmail.com>
2022-07-28 20:45:05 +0800

    ada penambahan

--- components/CardPorto.js
+++ components/CardPorto.js
@@ -1,10 +1,10 @@
 import Image from 'next/image'
-import React from 'react'
+import porto1 from '../public/img/portfolio/coffe.jpg'
 
 export default function CardPorto() {
   return (
     <div className='w-full lg:w-1/2 px-2 text-center py-3'>
-      <Image src="/img/portfolio/coffe.jpg" height={230} width={450} layout="responsive" className='max-w-full rounded-lg' />
+      <Image src={porto1} height={230} width={450} layout="responsive" className='max-w-full rounded-lg' />
       <h4 className='text-2xl my-2 capitalize'>Lorem ipsum dolor sit amet consectetur.</h4>
       <div className='text-slate-500'>Lorem ipsum dolor sit.</div>
     </div>
