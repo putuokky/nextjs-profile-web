4a220181c8132ff51bc4eca3d3048e804f6b4cb3
Okky Maheswara <okkymahes@gmail.com>
2022-08-25 09:55:26 +0800

    tambah file referensi

--- sections/Footer.js
+++ sections/Footer.js
@@ -2,8 +2,8 @@ import Hero from "@/components/Hero";
 
 export default function Footer() {
   return (
-    <footer className="py-6 text-center text-gray-500 dark:text-gray-400 text-sm">
-      <span className="dark:text-gray-100 text-gray-900 font-bold text-md mr-2 capitalize"><Hero /></span>&copy; {new Date().getFullYear()} All Right Reserved
+    <footer className="py-6 text-sm text-center text-gray-500 dark:text-gray-400">
+      <span className="mr-2 font-bold text-gray-900 capitalize dark:text-gray-100 text-md"><Hero /></span>&copy; {new Date().getFullYear()} All Right Reserved
     </footer>
   )
 }
